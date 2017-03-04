0542faf95fb2ed5c89210a11c2bcdc5e07965b7b
root <root@localhost.localdomain>
2017-03-04 22:39:03 +0530

    mod sql query file

--- queries.sql
+++ queries.sql
@@ -3,9 +3,12 @@ CREATE TABLE login
 id int NOT NULL AUTO_INCREMENT,
 tname varchar(255) NOT NULL,
 cname varchar(255) NOT NULL,
-email varchar(40),
-mno varchar(12),
-pass varchar(12),
-lang varchar(10),
+email varchar(40) NOT NULL,
+mno varchar(12) NOT NULL,
+pass varchar(12) NOT NULL,
+lang varchar(10) NOT NULL,
 PRIMARY KEY (ID)
-)
+);
+
+ ALTER TABLE login AUTO_INCREMENT=1000; 
+INSERT INTO login (tname,cname,email,mno,pass,lang) VALUES('ATM','TKM','name@gmail','7560881699','banana','cpp');
