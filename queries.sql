d5f56edecc8c7bc92eeaf983e9d59bacf8342767
root <root@localhost.localdomain>
2017-03-04 22:30:07 +0530

    Added sql query file

---
+++ queries.sql
@@ -0,0 +1,11 @@
+CREATE TABLE login
+(
+id int NOT NULL AUTO_INCREMENT,
+tname varchar(255) NOT NULL,
+cname varchar(255) NOT NULL,
+email varchar(40),
+mno varchar(12),
+pass varchar(12),
+lang varchar(10),
+PRIMARY KEY (ID)
+)
