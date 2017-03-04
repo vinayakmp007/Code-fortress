e3c1dfeeabf4daa9b4453ac2efa5210068057b26
root <root@localhost.localdomain>
2017-03-04 22:51:18 +0530

    mod sql query file

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 CREATE TABLE login
 (
 id int NOT NULL AUTO_INCREMENT,
-tname varchar(255) NOT NULL,
+tname varchar(255) NOT NULL UNIQUE,
 cname varchar(255) NOT NULL,
 email varchar(40) NOT NULL,
 mno varchar(12) NOT NULL,
@@ -12,3 +12,7 @@ PRIMARY KEY (ID)
 
  ALTER TABLE login AUTO_INCREMENT=1000; 
 INSERT INTO login (tname,cname,email,mno,pass,lang) VALUES('ATM','TKM','name@gmail','7560881699','banana','cpp');
+
+ CREATE TRIGGER namecheck BEFORE INSERT ON login FOR EACH ROW 
+BEGIN
+		
