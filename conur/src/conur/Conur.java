22c775dc475b81f81070de4a4bbb96adf91f88f1
root <root@localhost.localdomain>
2017-03-05 13:52:45 +0530

    quer+netbn

---
+++ conur/src/conur/Conur.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conur;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author root
+ */
+public class Conur {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       mn.main(args);
+    }
+    
+}
--- queries.sql
+++ queries.sql
@@ -3,9 +3,9 @@ CREATE TABLE team
 id int NOT NULL AUTO_INCREMENT,
 tname varchar(255) NOT NULL UNIQUE,
 cname varchar(255) NOT NULL,
-email varchar(40) NOT NULL,
-mno varchar(12) NOT NULL,
-pass varchar(12) NOT NULL,
+email varchar(40) NOT NULL UNIQUE,
+mno varchar(12) NOT NULL UNIQUE,
+pass varchar(12) NOT NULL UNIQUE,
 lang varchar(10) NOT NULL,
 PRIMARY KEY (ID)
 );
@@ -21,3 +21,21 @@ INSERT INTO login (tname,cname,email,mno,pass,lang) VALUES('ATM','TKM','name@gma
 BEGIN
 		
 >>>>>>> 1ab6e6e3b8d934535d5d53337e0cb425eb847ca1
+
+CREATE TABLE testcase
+(
+testid int NOT NULL AUTO_INCREMENT UNIQUE,
+tlevel  int NOT NULL ,
+qno    int NOT NULL,
+ncase int NOT NULL,
+tinput BLOB NOT NULL,
+toutput BLOB NOT NULL,
+PRIMARY KEY (testid)
+);
+ALTER TABLE testcase AUTO_INCREMENT=5000;
+
+
+INSERT INTO testcase (tlevel,qno,tinput,toutput) values(1,1,'aaaaa','bbbb');
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '1234' WITH GRANT OPTION;
+
