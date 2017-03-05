60a366c3e114a23f0bbae5eb94c96fb19b1d8b06
Vinayak MP <vinayakmp007@gmail.com>
2017-03-05 21:23:46 +0530

    Added js files

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('.card-content').readmore({
+  speed: 75,
+  maxHeight: 20,
+  moreLink: '<a class="readMore" href="#">Read more</a>',
+  lessLink: '<a class="readLess" href="#">Read less</a>'
+});
