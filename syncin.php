af826a68614e7e5a26514a4a164c5d083cc6a449
Vinayak MP <vinayakmp007@gmail.com>
2017-03-11 19:28:54 +0530

    Completed

--- syncin.php
+++ syncin.php
@@ -17,22 +17,60 @@ if(mysqli_num_rows($retval)==1){                                                
 $row = mysqli_fetch_assoc($retval);
 if($row['status']==0&&isset($_POST['OK'])&&isset($_SESSION['teamid'])){
 
+
+
+if((!isset($_POST['level']))||(!isset($_POST['qstnno'])))die("ERR:142");
 $level=$_POST['level'];
 $team=$_SESSION['teamid'];
 $qno=$_POST['qstnno'];
 $dat=$_POST['dat'];
 
 
+$qry="select * from sync where tlevel=$level and teamid=$team"; 
+$ret4 =mysqli_query($conn, $qry);
+
+//echo $qry;
+if(!$ret4)die("ERR:13");
+if(mysqli_num_rows($ret4)==1){                        //if it contains then change
+
+
 $null=NULL;
-$stmt = $conn->prepare("update sync (tlevel,qno,teamid,dat) values(?,?,?,?) where tlevel=$level and teamid=$team and qno=$qno");
+$stmt = $conn->prepare("update sync set dat=? where tlevel=$level and teamid=$team and qno=$qno");
+$stmt->bind_param("b",$null);
+$stmt->send_long_data(0,$dat);
+
+$stmt->execute();                   //TODO          check whether error is empty
+
+
+if(!empty($stmt->error))die("ERR:152");
+$stmt->close(); 
+}
+else if(mysqli_num_rows($ret4)==0){                                      //not added
+
+
+
+$null=NULL;
+$stmt = $conn->prepare("insert into sync (tlevel,qno,teamid,dat) values(?,?,?,?) ");
+
+if(!$stmt)die("ERR:561");
+
 $stmt->bind_param("dddb",$level,$qno,$team,$null);
 $stmt->send_long_data(3,$dat);
 
 $stmt->execute();                   //TODO          check whether error is empty
-$stmt->close(); 
+ 
+
+if(!empty($stmt->error))die("ERR:153");
+$stmt->close();
+}
+else die("ERR:14");
 
 
 
+
+
+echo 'OK';
+die();
 }else die("ERR:85");
 
 }
